f07725d7c9ec58b9b315e084eefdbb4d7cf5aa79
Ceri-Jane <ceri.jane.89@gmail.com>
2025-04-18 17:00:40 +0100

    Set up basic javascript functions

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+funtion runGame() {
+
+}
+
+funtion checkAnswer() {
+    
+}
+
+funtion calculateCorrectAnswer() {
+    
+}
+
+funtion incrementScore() {
+    
+}
+
+funtion incrementWrongAnswer() {
+    
+}
+
+funtion displayAdditionQuestion() {
+    
+}
+
+funtion displaySubtractQuestion() {
+    
+}
+
+funtion displayMultiplyQuestion() {
+    
+}
